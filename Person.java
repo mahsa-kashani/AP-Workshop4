5c54007924dffeac2d999c0bdc788f541489b642
mahsa-kashani <mahsa12.kashani@gmail.com>
2023-04-28 00:55:08 +0430

    Update Person.java
    getters added

--- Person.java
+++ Person.java
@@ -1,4 +1,12 @@
 public class Person {
     private String firstName;
     private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
