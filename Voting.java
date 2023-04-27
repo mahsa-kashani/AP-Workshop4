c567e671bf50c119aefeac196f1cccad7a3df4bb
mahsa-kashani <mahsa12.kashani@gmail.com>
2023-04-28 01:34:08 +0430

    Update Voting.java
    fields added

--- Voting.java
+++ Voting.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Voting {
+    private int type;
+    private String question;
+    private HashMap<String , HashSet<Vote>> choices;
+    private boolean isAnonymous;
+    private ArrayList<Person> voters;
 }
