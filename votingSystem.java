95872938743955f3d57b174b15b20efab0e1aef6
mahsa-kashani <mahsa12.kashani@gmail.com>
2023-04-28 13:32:24 +0430

    Update votingSystem.java
    constructor added

--- votingSystem.java
+++ votingSystem.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 
 public class votingSystem {
     private ArrayList<Voting> votingList;
+
+    public votingSystem(ArrayList<Voting> votingList) {
+        this.votingList = votingList;
+    }
 }
